dbc46b1b897269b4a5f203c8571ab08e5c547c80
DenZel1502 <denzel.zuniga@tecsup.edu.pe>
2023-11-03 13:21:06 -0500

    Initial Commit

---
+++ Airbnb-Clone-A/Airbnb_Clone_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_Clone_AApp.swift
+//  Airbnb-Clone-A
+//
+//  Created by MAC43 on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_Clone_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
